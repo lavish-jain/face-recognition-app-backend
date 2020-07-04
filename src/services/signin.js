3a291437c399d949ce6a0f6b84382a79611439fc
lavish-jain <lavishrjain1997@gmail.com>
2020-07-04 19:43:05 +0530

    Changed response of /signin from 'SUCCESS' / 'FAILURE' to / 'UNAUTHORISED'

--- src/services/signin.js
+++ src/services/signin.js
@@ -1,9 +1,9 @@
 const signin = (email, password) => {
     const matchedEntry = database.users.filter(user => user.email === email && user.password === password);
     if (matchedEntry.length === 1) {
-        return {status: 200, response:'SUCCESS'};
+        return {status: 200, response: matchedEntry[0]};
     } else {
-        return {status: 401, response:'FAILURE'};
+        return {status: 401, response:'UNAUTHORISED'};
     }
 }
 
