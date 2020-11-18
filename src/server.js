0d0faf0a637126b6079b45b1ce90a8ce141d7d6f
lavish-jain <lavishrjain1997@gmail.com>
2020-11-19 03:38:38 +0530

    changed db vars

--- src/server.js
+++ src/server.js
@@ -16,7 +16,12 @@ try {
 
     const db = knex({
         client: 'pg',
-        connection: process.env.DATABASE_URL,
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        },
         ssl: {
             rejectUnauthorized: false
         }
